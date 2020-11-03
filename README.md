1683e9262a25cc2caa0046e852ab1114045881c4
Jreynolds97 <Jreynolds97@comcast.net>
2020-11-03 10:57:08 -0700

    Rework the calculation to get rid of random

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## No Bow RNG
-Bukkit/Spigot plugin to mitigate the randomness of arrows fired in Minecraft.
+Bukkit/Spigot plugin to remove randomness from arrows fired in Minecraft.
 
 `mvn clean package` to build the jar from sources
--- src/main/java/com/jreynolds97/plugin/bowListener.java
+++ src/main/java/com/jreynolds97/plugin/bowListener.java
@@ -1,32 +1,37 @@
 package com.jreynolds97.plugin;
 
-import org.bukkit.entity.Entity;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.util.Vector;
 
 public class bowListener implements Listener{
     @EventHandler
-    public void onProjectileFire(ProjectileLaunchEvent event){
-        Entity entity = event.getEntity();
-        if(event.getEntityType() == EntityType.ARROW && (event.getEntity().getShooter() instanceof Player)){
-            Player shooter = (Player) event.getEntity().getShooter();
+    public void onBowFire(EntityShootBowEvent event){
+        if (event.getEntityType().equals(EntityType.PLAYER)){
+            Player shooter = (Player) event.getEntity();
+            Location loc = shooter.getLocation();
+            Double force = Double.valueOf(event.getForce()) * 3.0F;
+
+            Double motX = (double) (-Math.sin(Math.toRadians(loc.getYaw())) * Math.cos(Math.toRadians(loc.getPitch())));
+            Double motZ = (double) (Math.cos(Math.toRadians(loc.getYaw())) * Math.cos(Math.toRadians(loc.getPitch())));
+            Double motY = (double) (-Math.sin(Math.toRadians(loc.getPitch())));
             
-            // Get the spherical representation of the coordinates. Have to offset by 90 because Minecraft is backwards
-            Double Theta = Math.toRadians(shooter.getLocation().getYaw() + 90);
-            Double Phi = Math.toRadians(shooter.getLocation().getPitch() + 90);
-            Double Rho = entity.getVelocity().length();
+            Double f2 = Math.sqrt(motX * motX + motZ * motZ + motY * motY);
 
-            // Transform into Cartesian coordinates to use in minecraft
-            Double newX = Rho * Math.sin(Phi) * Math.cos(Theta);
-            Double newZ = Rho * Math.sin(Phi) * Math.sin(Theta);
-            Double newY = Rho * Math.cos(Phi);
+            motX /= f2;
+            motZ /= f2;
+            motY /= f2;
+            // The bukkit 1.7.10 code has a source of random here, by removing it and replicating the rest of the code
+            // we can force all arrows to travel along the same vector given constant facing and bow draw
+            motX *= force;
+            motY *= force;
+            motZ *= force;
 
-            // Overwrite the velocity with the new vector
-            entity.setVelocity(new Vector(newX, newY, newZ));
+            event.getProjectile().setVelocity(new Vector(motX, motY, motZ));
         }
     }
 }
